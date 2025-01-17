d61faf92f844335942de57699a5f005c3b15fccb
Мизин Сергей Юрьевич (4091265) <seeergo@yahoo.com>
2025-01-17 20:47:48 +0400

    mess

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,23 +10,74 @@ export enum InputParamType {
   QUERY = 'query',
   PATH = 'path',
 }
-const getPropetiesFromValueType = (value: Exclude<OpenAPIV3_1.SchemaObject, boolean>) => {
+
+export interface ParsedParam {
+  paramName: string
+  paramType?: InputParamType
+  description: string
+  required: boolean
+  schema: string
+  schemaComment: string
+} 
+const notBoolean = <T>(value: T): value is Exclude<T, boolean> => typeof value !== 'boolean'
+const getPropetiesFromValueType = (data: OpenAPIV3_1.Document, value: Exclude<OpenAPIV3_1.SchemaObject, boolean>): any => {
   if(value.type === 'array') {
-    return []
+    return parseSchema(data, value.items)
   } else if (Array.isArray(value.type)) {
     return []
   } else {
-    return []
+    const paramsArray = Object.values(value.properties ?? {}).filter(notBoolean)
+    return parseParameters(paramsArray)
   }
 }
 
-const parseSchema = (schema: OpenAPIV3_1.SchemaObject | OpenAPIV3_1.ReferenceObject) => {
-  
+const parsePrimitiveSchema = (schema: OpenAPIV3_1.NonArraySchemaObject): string => `${schema.type}${schema.format ? `($${schema.format})` : ''}`
+const parsePrimitiveArraySchema = (schema: OpenAPIV3_1.ArraySchemaObject): string => `Array(${parsePrimitiveSchema(schema.items)})`
+
+const parseSchema = (data: OpenAPIV3_1.Document, schema: OpenAPIV3_1.SchemaObject | OpenAPIV3_1.ReferenceObject) => {
+  if (typeof schema !== 'object') return undefined
+  if ('$ref' in schema) {
+    const referenceSchema = findSchema(data, getSchemaNameFromRef(schema.$ref))
+    return referenceSchema ? parseSchema(data, referenceSchema) : undefined
+  }
+  return getPropetiesFromValueType(data, schema)
 }
-const findSchema = (data: OpenAPIV3_1.Document, schemaName: string) => {
+const getSchemaNameFromRef = (ref: string) => ref.split('/').at(-1)
+const findSchema = (data: OpenAPIV3_1.Document, schemaName?: string) => {
+  if(!schemaName) return undefined
   const schemaKey = data.components?.schemas ? Object.keys(data.components.schemas).find((key) => key === schemaName) : undefined
   return schemaKey && data.components?.schemas ? data.components?.schemas[schemaKey] : undefined
 }
+const parsePrimitiveProperty = (param: OpenAPIV3_1.NonArraySchemaObject, required: boolean): ParsedParam => {
+  return {
+    paramName: param.title ?? 'Нет названия',
+    paramType: undefined,
+    description: param.description ?? 'Нет описания',
+    required,
+    schema: parsePrimitiveSchema(param),
+    schemaComment: ''
+  } 
+}
+const parsePrimitiveParam = (param: OpenAPIV3_1.ParameterObject): ParsedParam => {
+  return {
+    paramName: param.name ?? 'Нет названия',
+    paramType: param.in as InputParamType,
+    description: param.description ?? 'Нет описания',
+    required: !!param.required,
+    schema: param.schema ? '$ref' in param.schema ? '' : parsePrimitiveSchema(param.schema) : '',
+    schemaComment: ''
+  } 
+}
+const parseParameters = (params: (OpenAPIV3_1.ArraySchemaObject | OpenAPIV3_1.NonArraySchemaObject | OpenAPIV3_1.MixedSchemaObject | OpenAPIV3_1.ReferenceObject)[]) => params.map((param) => '$ref' in param ? {
+  description: param.description,
+  paramName: param.$ref.split('/').at(-1)
+} : {
+  paramName: param.name,
+  paramType: param.in,
+  description: param.description,
+  required: param.required,
+  schema: `${param.schema}`
+})
 export const parseData = (data?: OpenAPIV3_1.Document) => {
   // const parsedSchemas = data?.components?.schemas ? Object.entries(data.components.schemas).map(([key, value]) => typeof value === 'object' ? {
   //   name: key,
@@ -47,7 +98,7 @@ export const parseData = (data?: OpenAPIV3_1.Document) => {
   //   properties: []
   // }) : []
   // return parsedSchemas
-  return data?.paths ? Object.entries(data?.paths).flatMap(([key, value]) => value ? Object.entries(value).map(([method, methodDesc]) => typeof methodDesc === 'object' ? ({
+  return data?.paths ? Object.entries(data?.paths).flatMap(([key, value]) => value ? Object.entries(value).map(([method, methodDesc]) => typeof methodDesc === 'object' ? {
     path: key,
     method,
     methodDesc: 'summary' in methodDesc ? methodDesc?.summary : '',
@@ -61,7 +112,7 @@ export const parseData = (data?: OpenAPIV3_1.Document) => {
       required: param.required,
       schema: `${param.schema}`
     }) : []
-  }) : {
+  } : {
     path: key,
     method,
     inputParams: [],
