e64fd0ed75d1bceb596b4d4c8dfd57ad7ae9fbf5
Sergey Mizin <seeergo@yahoo.com>
2025-04-05 08:29:13 +0300

    route

---
+++ src/app/api/convert/route.ts
@@ -0,0 +1,29 @@
+import { parseData } from '@/lib/utils'
+import { convertToDocxContent } from '@/utils/convert-to-docx'
+import { Document, Packer } from 'docx'
+import yaml from 'js-yaml'
+
+export async function POST(request: Request) {
+  const { text, type } = await request.json()
+  let data
+
+  if (type === 'json') {
+    data = JSON.parse(text)
+  } else if (type === 'yaml') {
+    data = yaml.load(text)     
+  } else {
+    return Response.json('NOT OK')
+  }
+  const parsedData = parseData(data)      
+
+  const doc = new Document({
+    sections: [{
+      properties: {},
+      children: convertToDocxContent(parsedData)
+    }]
+  })
+
+  const buffer = await Packer.toBuffer(doc)
+  const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' })
+  return new Response(blob)
+}
